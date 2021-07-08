4788eb74492dca48aff8abd832712149a9168373
jddaye <86455385+jddaye@users.noreply.github.com>
2021-07-08 15:42:10 -0400

    Included height
    Fixed a typo in Bulbasaur's typing. Created a loop to add the pokemon's name with their height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: .7, type: ['Grass', 'Flying']},
+  {name: 'Bulbasaur', height: .7, type: ['Grass', 'Poison']},
   {name: 'Charmander', height: .6, type: 'Fire'},
   {name: 'Squirtle', height: .5, type: 'Water'},
-]
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height +') ');
+
+
+if (pokemonList[i].height <1.5 && pokemonList[i].height >.6){
+  document.write(" - Wow, that's big! ")
+}
+}
