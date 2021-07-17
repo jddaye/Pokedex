1029f8d716296f7d455a9c0ede4ac048dae68e79
jddaye <86455385+jddaye@users.noreply.github.com>
2021-07-17 19:49:40 -0400

    Updating for IIFE
    Updating for cleaner code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: .7, type: ['Grass', 'Poison']},
-  {name: 'Charmander', height: .6, type: 'Fire'},
-  {name: 'Squirtle', height: .5, type: 'Water'},
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: .7, type: ['Grass', 'Poison']},
+    {name: 'Charmander', height: .6, type: 'Fire'},
+    {name: 'Squirtle', height: .5, type: 'Water'},
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height +') ');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+document.write(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu'});
+document.write(pokemonRepository.getAll());
+
+// function pokemonLoop(pokemon) {
+  // document.write(pokemon.name + ' is ' + pokemon.height + 'm tall and is ' + pokemon.type + ' type.');
+// }
+// pokemonList.forEach(pokemonLoop)
 
 
-if (pokemonList[i].height <1.5 && pokemonList[i].height >.6){
-  document.write(" - Wow, that's big! ")
-}
-}
+ // if (pokemonList[i].height <1.5 && pokemonList[i].height >.6){
+  // document.write(" - Wow, that's big! ")
+//}
